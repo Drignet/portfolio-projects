fabc5a1f284a360af5a5f54df865485ceb3032bf
drignet <odibehenry@gmail.com>
2023-12-24 15:28:24 -0800

    update

--- projects/my-portfolio/portfolio/src/script.js
+++ projects/my-portfolio/portfolio/src/script.js
@@ -1,9 +1,13 @@
+import work1 from "./assets/work-1.png";
+import work2 from "./assets/work-2.png";
+import work3 from "./assets/work-3.png";
+
 const mobileIcon = document.querySelector(".menu-icon");
 const mainMenu = document.querySelector(".main-menu");
 
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
-const images = ["work-1.png", "work-2.png", "work-3.png"];
+const images = [work1, work2, work3];
 const imageProject = document.querySelector("#image-project");
 
 const date = document.querySelector(".date");
@@ -21,11 +25,11 @@ Array.from(mainMenu.children).forEach((child) => {
 let currentImage = 0;
 nextBtn.addEventListener("click", () => {
   currentImage = (currentImage + 1) % images.length;
-  imageProject.src = `./src/assets/${images[currentImage]}`;
+  imageProject.src = `${images[currentImage]}`;
 });
 prevBtn.addEventListener("click", () => {
   currentImage = (currentImage - 1 + images.length) % images.length;
-  imageProject.src = `./src/assets/${images[currentImage]}`;
+  imageProject.src = `${images[currentImage]}`;
 });
 
 date.innerHTML = getCurrentDate();
